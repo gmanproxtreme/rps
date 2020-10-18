bc5a2c5c3c5f8deb32438d1c32cbef940f78416d
GPXCoder <gavinjpeters@gmail.com>
2020-10-19 00:52:00 +1000

    Added full labels for players too

--- rps.py
+++ rps.py
@@ -19,18 +19,21 @@ ComputerType = math.floor(random.random()*3)  #random.random(3)
 print("you picked ",StrUserType.upper())
 print("The computer picked ",ComputerType)
 
-if StrUserType.upper() == "P":
-    if ComputerType == 0: Draw()
-    elif ComputerType == 1: PWin()
-    elif ComputerType == 2: CWin()
-elif StrUserType.upper() == "R":
-    if ComputerType == 1: Draw()
-    elif ComputerType == 2: PWin()
-    elif ComputerType == 0: CWin()
-elif StrUserType.upper() == "S":
-    if ComputerType == 2: Draw()
-    elif ComputerType == 0: PWin()
-    elif ComputerType == 1: CWin()
+if ComputerType == 0:
+    print("The computer picked Paper")
+    if StrUserType.upper() == "P": Draw()
+    elif StrUserType.upper() == "R": CWin()
+    elif StrUserType.upper() == "S": PWin()
+elif ComputerType == 1:
+    print("The computer picked Rock")
+    if StrUserType.upper() == "P": PWin()
+    elif StrUserType.upper() == "R": Draw()
+    elif StrUserType.upper() == "S": CWin()
+elif ComputerType == 2:
+    print("The computer picked Scissors")
+    if StrUserType.upper() == "P": CWin()
+    elif StrUserType.upper() == "R": PWin()
+    elif StrUserType.upper() == "S": Draw()
 else:
     print("You need to pick a choice by typing the first letter only.")
     exit
