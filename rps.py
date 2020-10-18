b8aa9e51545161886e2a3541dc1639839f029ef3
GPXCoder <gavinjpeters@gmail.com>
2020-10-19 00:59:20 +1000

    Switched back to User's choice as primary If loops, Complete)

--- rps.py
+++ rps.py
@@ -16,25 +16,38 @@ StrUserType = input("Select Paper, Rock or Scissors (P,R or S)")
 ComputerType = math.floor(random.random()*3)  #random.random(3)
 # 0==Paper, 1==Rock and 2==Scissors
 
-print("you picked ",StrUserType.upper())
-print("The computer picked ",ComputerType)
-
-if ComputerType == 0:
-    print("The computer picked Paper")
-    if StrUserType.upper() == "P": Draw()
-    elif StrUserType.upper() == "R": CWin()
-    elif StrUserType.upper() == "S": PWin()
-elif ComputerType == 1:
-    print("The computer picked Rock")
-    if StrUserType.upper() == "P": PWin()
-    elif StrUserType.upper() == "R": Draw()
-    elif StrUserType.upper() == "S": CWin()
-elif ComputerType == 2:
-    print("The computer picked Scissors")
-    if StrUserType.upper() == "P": CWin()
-    elif StrUserType.upper() == "R": PWin()
-    elif StrUserType.upper() == "S": Draw()
+if StrUserType.upper() == "P": 
+    print("You picked Paper")
+    if ComputerType == 0:
+        print("The computer picked Paper")
+        Draw()
+    elif ComputerType == 1:
+        print("The computer picked Rock")
+        PWin()    
+    elif ComputerType == 2:
+        print("The computer picked Scissors")
+        CWin()
+elif StrUserType.upper() == "R": 
+    print("You picked Rock")
+    if ComputerType == 0:
+        print("The computer picked Paper")
+        CWin()
+    elif ComputerType == 1:
+        print("The computer picked Rock")
+        Draw()    
+    elif ComputerType == 2:
+        print("The computer picked Scissors")
+        PWin()
+elif StrUserType.upper() == "S": 
+    print("You picked Scissors")
+    if ComputerType == 0:
+        print("The computer picked Paper")
+        PWin()
+    elif ComputerType == 1:
+        print("The computer picked Rock")
+        Cwin()    
+    elif ComputerType == 2:
+        print("The computer picked Scissors")
+        Draw()
 else:
     print("You need to pick a choice by typing the first letter only.")
-    exit
-
